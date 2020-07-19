32c4bb43bd9b9f4291642d06116b892688ff9718
Gabriel Alencar <gabriel_alencar2@hotmail.com>
2020-07-19 17:17:11 -0300

    Listando produtos adicionados no carrinho; Aumentando e diminuindo quantidades dos produtos no carrinho

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,23 +39,25 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((acc, cur) => {
+      const itemTotal = cur.quantity * cur.price;
 
-    return formatValue(0);
+      return acc + itemTotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    return products.reduce((acc, cur) => acc + cur.quantity, 0);
   }, [products]);
 
   return (
@@ -68,7 +70,7 @@ const Cart: React.FC = () => {
           ListFooterComponentStyle={{
             height: 80,
           }}
-          renderItem={({ item }: { item: Product }) => (
+          renderItem={({ item }) => (
             <Product>
               <ProductImage source={{ uri: item.image_url }} />
               <ProductTitleContainer>
